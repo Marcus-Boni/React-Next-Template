9eb5eea970fad74ae1f79097c18c87bcd8928417
Marcus <mgalvaoboni@gmail.com>
2023-08-08 21:48:26 -0300

    testing if is working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <h1 className="text-4xl text-center text-blue-500">Hello World</h1>
+    <div>
+      <h1 className="text-4xl text-center text-blue-500">Hello World</h1>
+      <h2 className="text-2xl text-center text-sky-400"></h2>
+    </div>
   )
 }
